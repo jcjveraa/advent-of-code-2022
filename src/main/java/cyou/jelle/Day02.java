e0795fddfc927abe53bde83fde615476e1205c89
Jelle Veraa <jveraa@ilionx.com>
2022-12-01 14:47:34 +0100

    test multiple remotes

---
+++ src/main/java/cyou/jelle/Day02.java
@@ -0,0 +1,10 @@
+package cyou.jelle;
+
+import cyou.jelle.util.InputProcessor;
+
+public class Day02 {
+    public static void main(String[] args) {
+        var lines = InputProcessor.loadLines("src/main/resources/Day02.input.txt");
+
+    }
+}
