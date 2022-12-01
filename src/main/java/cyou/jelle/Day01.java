4b9e073649f4382d62fd74e83f87795dc6cc0ff4
Jelle Veraa <jveraa@ilionx.com>
2022-12-01 09:26:05 +0100

    Bit of refactoring on day 1

--- src/main/java/cyou/jelle/Day01.java
+++ src/main/java/cyou/jelle/Day01.java
@@ -1,37 +1,40 @@
 package cyou.jelle;
 
+import cyou.jelle.util.Printer;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.stream.Collectors;
 
 public class Day01 {
     public static void main(String[] args) throws IOException {
         try (var lines = Files.lines(Path.of("src/main/resources/Day01.input.txt"))) {
 
-            var integers = lines.map(line -> (line.length() > 0) ? Integer.parseInt(line) : null).toList();
+            var integers = lines
+                    .map(line -> (line.length() > 0) ? Integer.parseInt(line) : Integer.MIN_VALUE)
+                    .toList();
 
             var sums = new ArrayList<Integer>();
-            Integer currentSum = 0;
-            for(var value: integers) {
-                if(value == null) {
+            int currentSum = 0;
+            for (var value : integers) {
+                if (value == Integer.MIN_VALUE) {
                     sums.add(currentSum);
                     currentSum = 0;
-                }
-                 else {
-                     currentSum += value;
+                } else {
+                    currentSum += value;
                 }
             }
 
-            System.out.println(sums.stream().max(Integer::compareTo).get());
+            Integer maxValue = sums.stream().max(Integer::compareTo).orElseGet(() -> -1);
+            Printer.println(maxValue);
 
             var maxThree = sums.stream()
                     .sorted(Comparator.comparing(Integer::intValue).reversed())
-                    .toList().subList(0,3)
-                    .stream().reduce(Integer::sum).get();
-            System.out.println(maxThree);
+                    .limit(3)
+                    .reduce(Integer::sum).orElseGet(() -> -1);
+           Printer.println(maxThree);
 
         }
     }
---
+++ src/main/java/cyou/jelle/util/Printer.java
@@ -0,0 +1,8 @@
+package cyou.jelle.util;
+
+public class Printer {
+    private Printer(){}
+    public static void println(Object o) {
+        System.out.println(o);
+    }
+}
