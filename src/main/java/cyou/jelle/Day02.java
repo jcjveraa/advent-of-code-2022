94339de9490f5bb0735537dbd6cdd9856261ce0f
Jelle Veraa <jveraa@ilionx.com>
2022-12-02 08:44:52 +0100

    Cleaned up

--- src/main/java/cyou/jelle/Day02.java
+++ src/main/java/cyou/jelle/Day02.java
@@ -14,13 +14,12 @@ public class Day02 {
         for (var line : lines) {
             char theirs = line[0].charAt(0);
             char mine = line[1].charAt(0);
-            counter += myValue(mine)+1;
-            counter += matchPoints(mine, theirs);
+            counter += myValue(mine) + 1;
+            counter += firstMatchPoints(mine, theirs);
 
             char roundOutcome = mine;
-            counterStar2 += roundScoreSecondStar(roundOutcome);
-            counterStar2 += myPickSecondStar(theirs, roundOutcome);
-
+            counterStar2 += secondMatchPoints(roundOutcome);
+            counterStar2 += myValueSecondStar(theirs, roundOutcome) + 1;
         }
 
         Printer.println("1: " + counter);
@@ -28,39 +27,31 @@ public class Day02 {
     }
 
     static int myValue(char mySelection) {
-        return mySelection - 'X' + 1;
+        return mySelection - 'X';
     }
 
     static int theirValue(char theirSelection) {
-        return theirSelection - 'A' + 1;
+        return theirSelection - 'A';
     }
 
-    static int matchPoints(char mine, char theirs) {
+    static int firstMatchPoints(char mine, char theirs) {
         int myValue = myValue(mine);
         int theirValue = theirValue(theirs);
 
         if (myValue == theirValue) return 3;
-        if (myValue == (theirValue % 3 + 1)) return 6;
-
+        if (myValue == (theirValue + 1) % 3) return 6;
         return 0;
     }
 
-    static int roundScoreSecondStar(char outcome) {
-        return (myValue(outcome) - 1) * 3;
+    static int secondMatchPoints(char outcome) {
+        return myValue(outcome) * 3;
     }
 
-    static int myPickSecondStar(char theirs, char roundResult) {
+    static int myValueSecondStar(char theirs, char roundResult) {
         int theirValue = theirValue(theirs);
-        int outcome = roundScoreSecondStar(roundResult);
-        if (outcome == 3) {
-            return theirValue;
-        }
 
-        if (outcome == 6) {
-            return (theirValue % 3) + 1;
-        }
-
-        if (theirValue != 1) return theirValue - 1;
-        return 3;
+        if (roundResult == 'Y') return theirValue;
+        if (roundResult == 'Z') return ((theirValue + 1) % 3);
+        return Math.floorMod(theirValue - 1, 3);
     }
 }
