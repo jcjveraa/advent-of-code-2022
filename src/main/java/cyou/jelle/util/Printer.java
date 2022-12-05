b1c251a81b1b6435efdd12875a4f9fa84e7fa05b
Jelle Veraa <jveraa@ilionx.com>
2022-12-05 10:05:45 +0100

    Clarify Printer

--- src/main/java/cyou/jelle/util/Printer.java
+++ src/main/java/cyou/jelle/util/Printer.java
@@ -2,6 +2,8 @@ package cyou.jelle.util;
 
 public class Printer {
     private Printer(){}
+    // This exists just to trick my IDE to stop complaining all the time use of the System.out, without
+    // actually using an external logging library.
     public static void println(Object o) {
         System.out.println(o);
     }
