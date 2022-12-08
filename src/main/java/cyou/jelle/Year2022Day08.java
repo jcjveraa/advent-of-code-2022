f3e680338121811a6ccbc896df281b42098307dd
Jelle Veraa <jveraa@ilionx.com>
2022-12-08 09:37:15 +0100

    2022 day 8 star 2

--- src/main/java/cyou/jelle/Year2022Day08.java
+++ src/main/java/cyou/jelle/Year2022Day08.java
@@ -17,31 +17,86 @@ public class Year2022Day08 {
         loadData();
 
         var treecounter = 0;
+        var maxScore = 0;
         width = integers.length;
         height = integers[0].length;
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                if (i == 0 || j == 0 || i == integers.length - 1 || j == integers[0].length - 1 || treeSticksOut(i, j))  {
+                if (i == 0 || j == 0 || i == integers.length - 1 || j == integers[0].length - 1 || treeSticksOut(i, j)) {
                     treecounter++;
                 }
 
+                int score = treesVisibleScore(i, j);
+                if (score > maxScore) {
+                    maxScore = score;
+                }
             }
         }
         Printer.println(treecounter);
+        Printer.println(maxScore);
 
+
+    }
+
+    private static int treesVisibleScore(int i, int j) {
+        var left = getLeft(i, j);
+        ArrayUtils.reverse(left);
+        var right = getRight(i, j);
+        var up = getUp(i, j);
+        ArrayUtils.reverse(up);
+        var down = getDown(i, j);
+
+        var score = 1;
+        var tree = integers[i][j];
+        score *= countVisibleTrees(left, tree);
+        score *= countVisibleTrees(right, tree);
+        score *= countVisibleTrees(up, tree);
+        score *= countVisibleTrees(down, tree);
+        return score;
+    }
+
+    private static int countVisibleTrees(Integer[] otherTrees, int tree) {
+        int result = 0;
+        for (int i = 0; i < otherTrees.length; i++) {
+            var otherTree = otherTrees[i];
+            if (otherTree >= tree) {
+                result = i+1;
+                break;
+            }
+            if(i == otherTrees.length-1) {
+                result = otherTrees.length;
+            }
+        }
+        return result;
     }
 
     private static boolean treeSticksOut(int i, int j) {
         var tree = integers[i][j];
-        var left = ArrayUtils.subarray(getRow(i), 0, j);
-        var right = ArrayUtils.subarray(getRow(i), j + 1, width);
+        var left = getLeft(i, j);
+        var right = getRight(i, j);
         var leftRightVisible = isVisible(tree, left) || isVisible(tree, right);
 
-        var up = ArrayUtils.subarray(getCol(j), 0, i);
-        var down = ArrayUtils.subarray(getCol(j), i + 1, height);
+        var up = getUp(i, j);
+        var down = getDown(i, j);
         var upDownVisible = isVisible(tree, up) || isVisible(tree, down);
         return leftRightVisible || upDownVisible;
+    }
+
+    private static Integer[] getDown(int i, int j) {
+        return ArrayUtils.subarray(getCol(j), i + 1, height);
+    }
+
+    private static Integer[] getUp(int i, int j) {
+        return ArrayUtils.subarray(getCol(j), 0, i);
+    }
+
+    private static Integer[] getRight(int i, int j) {
+        return ArrayUtils.subarray(getRow(i), j + 1, width);
+    }
+
+    private static Integer[] getLeft(int i, int j) {
+        return ArrayUtils.subarray(getRow(i), 0, j);
     }
 
     private static boolean isVisible(Integer tree, Integer[] left) {
